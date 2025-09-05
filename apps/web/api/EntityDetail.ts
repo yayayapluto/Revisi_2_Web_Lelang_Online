34ea6d7cb186db6cf104d45b1fbd188f0162015a
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-09-05 17:29:05 +0700

    chore: api fn init

---
+++ apps/web/api/EntityDetail.ts
@@ -0,0 +1,16 @@
+// api/objectTypes.ts
+import axios from "axios"
+import type {ApiResponse} from "@/types/apiResponse"
+
+
+export const GetEntityDetail = <T>({
+                                       entityName,
+                                       id
+                                   }: {
+    entityName: string
+    id: string
+}) => {
+    return axios
+        .get<ApiResponse<T>>(`http://127.0.0.1:8080/api/${entityName}/${id}`)
+        .then(res => res.data)
+}
