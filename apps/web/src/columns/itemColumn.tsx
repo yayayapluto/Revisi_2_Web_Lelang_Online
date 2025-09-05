647238c8668927e769bbd2b0082127ccedaab973
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-09-05 21:23:33 +0700

    feat: item list page

---
+++ apps/web/src/columns/itemColumn.tsx
@@ -0,0 +1,110 @@
+import type {ColumnDef} from "@tanstack/react-table";
+import type {ObjectType} from "@/types/objectType";
+import {DataTableColumnHeader} from "@/components/data-table-column-header";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/animate-ui/radix/dropdown-menu";
+import {EllipsisIcon} from "lucide-react";
+import {Link} from "@tanstack/react-router";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+import type {Item} from "@/types/item";
+
+export const ItemColumn: ColumnDef<Item>[] = [
+    {
+        accessorKey: "name",
+        header: ({column}) => (
+            <DataTableColumnHeader column={column} title={"Name"}/>
+        ),
+    },
+    {
+        accessorKey: "price",
+        header: "Price"
+    },
+    {
+        accessorKey: "deposit_price",
+        header: "Deposit Price"
+    },
+    {
+        accessorKey: "description",
+        header: "Description",
+    },
+    {
+        accessorKey: "object_type",
+        header: "Object Type",
+        cell : ({row}) => row.original.object_type.name
+    },
+    {
+        accessorKey: "file",
+        header: "Main Thumbnail",
+        cell : ({row}) => row.original.file.path
+    },
+    {
+        accessorKey: "created_at",
+        header: "Created At",
+        cell: ({row}) => new Date(Date.parse(row.original.created_at)).toUTCString(),
+    },
+    {
+        accessorKey: "updated_at",
+        header: "Updated At",
+        cell: ({row}) => new Date(Date.parse(row.original.updated_at)).toUTCString(),
+    },
+    {
+        id: "actions",
+        cell: ({row}) => {
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Button variant={"ghost"}><EllipsisIcon/></Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuSeparator/>
+                        <DropdownMenuItem>
+                            <Link to={"/dashboard/object-type/$id"} params={{id: `${row.original.id}`}}>View Details</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Link to={"/dashboard/object-type/$id/edit"} params={{id: `${row.original.id}`}}>Edit</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={e => {
+                            e.preventDefault()
+                        }}>
+                            <AlertDialog>
+                                <AlertDialogTrigger>
+                                    Delete
+                                </AlertDialogTrigger>
+                                <AlertDialogContent>
+                                    <AlertDialogHeader>
+                                        <AlertDialogTitle>Are you sure want to delete '{row.original.name}'?</AlertDialogTitle>
+                                        <AlertDialogDescription>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, perferendis!
+                                        </AlertDialogDescription>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
+                                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                        <AlertDialogAction>Delete</AlertDialogAction>
+                                    </AlertDialogFooter>
+                                </AlertDialogContent>
+                            </AlertDialog>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        }
+    }
+]
---
+++ apps/web/src/routes/dashboard/item/index.tsx
@@ -0,0 +1,39 @@
+import {createFileRoute, useNavigate} from '@tanstack/react-router'
+import {Button} from "@/components/ui/button";
+import {PlusCircleIcon} from "lucide-react";
+import {Skeleton} from "@/components/ui/skeleton";
+import {GenericDataTable} from "@/components/generic-data-table";
+import type {ObjectType} from "@/types/objectType";
+import {ObjectTypeColumn} from "@/columns/objectTypeColumn";
+
+export const Route = createFileRoute('/dashboard/item/')({
+    component: RouteComponent,
+})
+
+function RouteComponent() {
+    const entity = "Object Type"
+    const navigate = useNavigate()
+    return (
+        <div className={"space-y-4"}>
+            <div className="flex justify-between">
+                <h1 className="text-2xl font-semibold tracking-wide capitalize">
+                    {entity}
+                </h1>
+                <Button className={"cursor-pointer capitalize"}
+                        onClick={() => navigate({to: `${location.pathname.replace(`/list`, ``)}/create`})}>
+                    <PlusCircleIcon/> Add {entity}</Button>
+            </div>
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                {[1, 2, 3, 4].map(() => (<Skeleton className={"h-40 rounded-lg"}/>))}
+            </div>
+            <GenericDataTable<ObjectType>
+                entity="objectTypes"
+                columns={ObjectTypeColumn}
+                debounceDelay={200}
+                initialPageSize={10}
+                defaultSortBy="id"
+                currentEntity={entity}
+            />
+        </div>
+    )
+}
